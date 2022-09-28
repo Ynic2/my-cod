63f6d1b7d5420e7c36aa2674498b6d72285e21a3
Ynic2 <63044918+Ynic2@users.noreply.github.com>
2022-09-28 19:05:27 +0300

    Add files via upload
    Быки и коровы(отгадывает комьютер)

--- Source.cpp
+++ Source.cpp
@@ -1,93 +1,91 @@
 #include <iostream>
+#include <ctime>
 #include <vector>
 #include <cmath>
-#include <ctime>
 
 using namespace std;
 
 int main() {
-	//Создание рандомного числа
+	//Ввод числа пользователем
 	setlocale(LC_ALL, "ru");
-	srand(time(NULL));
 	size_t size;
-	int stop = 0;
+	int a,stop=0;
 	while (stop == 0) {
-		cout << "Введите количество разрядов:" << endl;
-		cin >> size;
-		if (size <= 9 && size > 0) {
+		size = 0;
+		cout << "Введите четырехзначное число:" << endl;
+		cin >> a;
+		int b = a;
+		while (b > 0) {
+			b = b / 10;
+			size++;
+		}
+		if (size == 4) {
 			stop = 1;
 		}
 	}
+	vector<int> input(size);
+	for (int i = size - 1; i >= 0; i--) {
+		input[i] = a % 10;
+		a /= 10;
+	}
+	//Конец ввода
+	//Пк отгадывает
 	vector<int> number(size);
-	number[0] = 1 + rand() % 8;
-	for (int i = 1; i < size; i++) {
-		int pos;
-		pos = rand() % 9 + 1;
-		number[i] = pos;
-	}
-	vector<int> number_ex(size);
-	number_ex = number;
-	int ok = 0;
-	while (ok == 0) {
-		int k = 0;
-		for (int i = 1; i < size; i++) {
-			for (int q = 0; q < size; q++) {
-				if (number[i] == number_ex[q] && i != q) {
-					number[i] = rand() % 9 + 1;
+	int bulls = 0, cows = 0;
+	for (int q = 1; q <= 9; q++) {
+		number[0] = q;
+		if (bulls == 4) {
+			break;
+		}
+		for (int w = 0; w <= 9; w++) {
+			number[1] = w;
+			if (bulls == 4) {
+				break;
+			}
+			for (int e = 0; e <= 9; e++) {
+				number[2] = e;
+				if (bulls == 4) {
+					break;
+				}
+				for (int r = 0; r <= 9; r++) {
+					number[3] = r;
+					vector<int> number_ex(size);
 					number_ex = number;
-				}
-				else {
-					k++;
-				}
-			}
-		}
-		if (k == (size-1)*size) {
-			ok = 1;
-		}
-	}
-	for (int i = 0; i < size; i++) {
-		cout << number[i];
-	}
-	cout << endl;
-	//Конец рандомного числа
-	//Ввод числа пользователем
-	int res = 0;
-	while (res==0){
-		int a;
-		int stop1 = 0;
-		while (stop1 == 0) {
-			cout << "Введите число с разрядом " << size << ":"<< endl;
-			cin >> a;
-			if (a >= pow(10,size-1) && a < pow(10,size) ){
-				stop1 = 1;
-			}
-		}
-		vector<int> input(size);
-		for (int i = (size-1); i >= 0; i--) {
-			input[i] = a % 10;
-			a = a / 10;
-		}
-		//Конец ввода
-		//Проверка и результаты
-		int bulls = 0;
-		int cows = 0;
-		for (int i = 0; i < size; i++) {
-			for (int q = 0; q < size; q++) {
-				if (number[i] == input[q] && i == q) {
-					bulls++;
-				}
-				else if (number[i] == input[q] && i != q) {
-					cows++;
+					int k=0;
+					for (int i = 0; i < size; i++) {
+						for (int x = 0; x < size; x++) {
+							if (number[i] == number_ex[x] && i != x) {
+								k++;
+							}
+							
+						}
+					}
+					if (k == 0) {
+						cout << number[0] << number[1] << number[2] << number[3];
+						for (int i = 0; i < size; i++) {
+							for (int x = 0; x < size; x++) {
+								if (number[i] == input[x] && i == x) {
+									bulls++;
+								}
+								if (number[i] == input[x] && i != x) {
+									cows++;
+								}
+							}
+
+						}
+						if (bulls == 4) {
+							cout << "Компьютер победил!" << endl;
+							break;
+						}
+						else {
+							cout << "Количество коров: " << cows << endl;
+							cout << "Количество быков: " << bulls << endl;
+							cows = 0;
+							bulls = 0;
+						}
+					}
 				}
 			}
-		}
-		if (bulls == size) {
-			cout << "Вы победили!!"<<endl;
-			res = 1;
-		}
-		else {
-			cout << "Количество коров: " << cows<<endl;
-			cout << "Количество быков: " << bulls<<endl;
 		}
 	}
 }
